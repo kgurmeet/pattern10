b986431405d42ee28bf9d413685be02a891661d5
Gurmeet Kaur <131999481+kgurmeet@users.noreply.github.com>
2023-09-02 13:53:30 +0530

    Create pattern10.java

---
+++ pattern10.java
@@ -0,0 +1,17 @@
+
+public class pattern10{
+    public static void main(String[] args) {
+        int k=6,y=6;
+        for(k=6;k>=1;k--)
+        {
+            for(y=k;y<=6;y++)
+            {
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
+
